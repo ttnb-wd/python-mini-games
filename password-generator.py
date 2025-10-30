f30490bef2b597bc269eb299254cdfe63d2a5bdb
ttnb-wd <maybelasttime9@gmail.com>
2025-10-30 12:26:54 +0630

    correct letters

--- password-generator.py
+++ password-generator.py
@@ -4,9 +4,9 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0','1', '2','3','4','5', '6', '7','8','9']
 symbols = ['!', '#','$', '%', '&', '(',')', '*', '+']
 
-letters_count = int(input("how many letters would you like in your password?\n"))
-numbers_count = int(input("how many numbers would you like in your password?\n"))
-symbols_count = int(input("how many symbols would you like in your password?\n"))
+letters_count = int(input("How many letters would you like in your password?\n"))
+numbers_count = int(input("How many numbers would you like in your password?\n"))
+symbols_count = int(input("How many symbols would you like in your password?\n"))
 
 letters_list = []
 numbers_list = []
